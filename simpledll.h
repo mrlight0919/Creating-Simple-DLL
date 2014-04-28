e348098ecc3f9ecc0e552792bc526aea1a022b49
Aswini S <ashumeow@live.com>
2014-04-28 20:31:45 +0530

    Update simpledll.h

--- simpledll.h
+++ simpledll.h
@@ -10,7 +10,7 @@
 #else
 #define export __declspec (dllexport)
 #endif
-export bool CALLBACK CenterTextA (HDC hdc, PREC prc, PCSTR pString);
+export bool CALLBACK CenterTextA (HDC hdc, PRECT prc, PCSTR pString);
 export bool CALLBACK CenterTextW (HDC hdc, PRECT prc, PCWSTR pString);
 #ifdef UNICODE
 #define CenterText CenterTextW
@@ -19,6 +19,35 @@ export bool CALLBACK CenterTextW (HDC hdc, PRECT prc, PCWSTR pString);
 #define CenterText CenterTextA
 #define TextOut TextOutA
 #endif
+/* GetTextExtentPoint32 syntax generated in CPP
+BOOL GetTextExtentPoint32(
+  _In_   HDC hdc,
+  _In_   LPCTSTR lpString,
+  _In_   int c,
+  _Out_  LPSIZE lpSize
+);
+*/
+//GetTextExtentPoint32A
+bool GetTextExtentPoint32A(HDC hdc, LPCTSTR lpString,int iLength, LPSIZE lpsize);
+
+//GetTextExtentPoint32W
+bool etTextExtentPoint32W(HDC hdc, LPCTSTR lpString, int iLength , LPSIZE lpsize);
+
+/* TextOut function in CPP
+BOOL TextOut(
+  _In_  HDC hdc,
+  _In_  int nXStart,
+  _In_  int nYStart,
+  _In_  LPCTSTR lpString,
+  _In_  int cchString
+);
+*/
+//TextOutA
+TextOutA (hdc, prc(right - left - size.cx)/2, prc(bottom - top - size.cy)/2, pString, iLength);
+
+//TextOutW
+TextOutW (hdc, prc(right - left - size.cx)/2, prc(bottom - top - size.cy)/2, pString, iLength);
+
 //test.c
 #ifdef __cplusplus
 #define export extern "C" __declspec (dllexport)
