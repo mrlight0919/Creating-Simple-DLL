b9dd869e9d76378af39362415af6e1ea88b132ea
Aswini S <ashumeow@live.com>
2014-04-29 00:11:41 +0530

    Update simpledll.c

--- simpledll.c
+++ simpledll.c
@@ -6,19 +6,19 @@ int WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, PVOID pvReserved)
 {
 	return TRUE;
 }
-export BOOL CALLBACK CenterTextA (HDC hdc, PRECT prc, LPCSTR pString)
+EXPORT BOOL CALLBACK CenterTextA(HDC hdc, PRECT prc, LPCSTR pString)
 {
 	int iLength;
 	LPSIZE lpsize;
 	iLength= lstrlenA(pString);
-	GetTextExtentPoint32A(HDC hdc, PCSTR pString ,int iLength , LPSIZE lpsize);
-	return TextOutA (hdc, prc(right - left - size.cx)/2, prc(bottom - top - size.cy)/2, pString, iLength);
+	GetTextExtentPoint32A(hdc, lpString, iLength, lpsize);
+	return TextOutA (hdc, prc((right-left-size.cx)/2,(bottom-top-size.cy)/2), pString, iLength);
 }
-export BOOL CALLBACK CenterTextW (HDC hdc, PRECT prc, PCWSTR pString)
+EXPORT BOOL CALLBACK CenterTextW(HDC hdc, PRECT prc, PCWSTR pString)
 	{
 	int iLength;
 	LPSIZE lpsize;
 	iLength= lstrlenW(pString);
-	GetTextExtentPoint32W (HDC hdc, LPTCSTR lpString ,int iLength , LPSIZE lpsize);
-	return TextOutW (hdc, prc(right - left - size.cx)/2, prc(bottom - top - size.cy)/2, pString, iLength);
+	GetTextExtentPoint32W(hdc, lpString, iLength, lpsize);
+	return TextOutW (hdc, prc((right-left-size.cx)/2,(bottom-top-size.cy)/2), pString, iLength);
 }
