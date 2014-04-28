8b5c055f59bb51062de893b96d528939a32827f2
Aswini S <ashumeow@live.com>
2014-04-29 00:45:56 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,18 +1,18 @@
 #include <windows.h>
 #include "simpledll.h"
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+int WINAPI WinMain (HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR szCmdLine,int iCmdShow)
 {
-     static TCHAR szAppName[] = TEXT ("StrProg");
-     wndclass.style         = CS_HREDRAW | CS_VREDRAW;
-     wndclass.lpfnWndProc   = WndProc;
-     wndclass.cbClsExtra    = 0;
-     wndclass.cbWndExtra    = 0;
-     wndclass.hInstance     = hInstance;
-     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION);
-     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW);
-     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH);
-     wndclass.lpszMenuName  = NULL;
-     wndclass.lpszClassName = szAppName;
+     static char szAppName = TEXT(("StrProg"),
+     wndclass.style         = CS_HREDRAW | CS_VREDRAW,
+     wndclass.lpfnWndProc   = WndProc,
+     wndclass.cbClsExtra    = 0,
+     wndclass.cbWndExtra    = 0,
+     wndclass.hInstance     = hInstance,
+     wndclass.hIcon         = LoadIcon (NULL, IDI_APPLICATION),
+     wndclass.hCursor       = LoadCursor (NULL, IDC_ARROW),
+     wndclass.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH),
+     wndclass.lpszMenuName  = NULL,
+     wndclass.lpszClassName = szAppName);
      
      if (!RegisterClass (&wndclass))
      {
@@ -20,14 +20,14 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine
           return 0 ;
      }
      
-     hwnd = CreateWindow (szAppName, TEXT ("Testing..."),
+     hwnd = CreateWindow(szAppName, TEXT ("Testing..."),
                           WS_OVERLAPPEDWINDOW,
                           CW_USEDEFAULT, CW_USEDEFAULT,
                           CW_USEDEFAULT, CW_USEDEFAULT,
                           NULL, NULL, hInstance, NULL);
      
-     ShowWindow (hwnd, iCmdShow);
-     UpdateWindow (hwnd);
+     ShowWindow(hwnd, iCmdShow);
+     UpdateWindow(hwnd);
      
      while (GetMessage (&msg, NULL, 0, 0))
      {
@@ -47,15 +47,11 @@ EXPORT BOOL CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPar
      switch(message)
      {
      case WM_PAINT:
-          hdc= BeginPaint(hwnd,&ps);
-          
+          hdc=BeginPaint(hwnd,&ps);
           GetClientRect(hwnd,&rect);
-          
           CenterText(hdc,&rect,TEXT("This string displayed by a DLL"));
-          
           EndPaint(hwnd,&ps);
           return 0;
-          
      case WM_DESTROY:
           PostQuitMessage(0);
           return 0;
