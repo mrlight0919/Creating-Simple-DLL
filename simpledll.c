10701a5bd89157e123d2158af089a81c5b3a03d9
Aswini S <ashumeow@live.com>
2014-04-28 16:47:54 +0530

    Create simpledll.c

---
+++ simpledll.c
@@ -0,0 +1,24 @@
+// This simple DLL project from AshuMeow is about creating a DLL for the Drawing Routine library module
+// Not directly copied... The concept of this simple project is inspired from the book of Charles Petzold
+#include<windows.h>
+#include "simpledll.h"
+int WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, PVOID pvReserved)
+{
+	return TRUE;
+}
+EXPORT BOOL CALLBACK CenterTextA(HDC hdc, PRECT prc, PCSTR pString)
+{
+	int iLength;
+	SIZE size;
+	iLength=lstrlenA(pString);
+	GetTextExtentPoint32A(hdc,pString,iLength,&size);
+	return TextOutA(hdc,(prc->right - prc->left - size.cx)/2,(prc->bottom - prc->top - size.cy)/2,pString,iLength);
+}
+EXPORT BOOL CALLBACK CenterTextW(HDC hdc, PRECT prc, PCSTR pString)
+	{
+	int iLength;
+	SIZE size;
+	iLength=lstrlenW(pString);
+	GetTextExtentPoint32W(hdc,pString,iLength,&size);
+	return TextOutW(hdc,(prc->right - prc->left - size.cx)/2,(prc->bottom - prc->top - size.cy)/2,pString,iLength);
+}
