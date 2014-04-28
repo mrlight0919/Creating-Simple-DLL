caf5999ce16b04a10a48420182f147e993db31a4
Aswini S <ashumeow@live.com>
2014-04-28 18:31:05 +0530

    Update simpledll.h

--- simpledll.h
+++ simpledll.h
@@ -1,3 +1,8 @@
+#include<windef.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include<conio.h>
 //simpledll.c
 #ifdef __cplusplus
 #define export extern "C" __declspec (dllexport)
@@ -8,8 +13,10 @@ export bool CALLBACK CenterTextA (HDC hdc, PREC prc, PCSTR pString);
 export bool CALLBACK CenterTextW (HDC hdc, PRECT prc, PCWSTR pString);
 #ifdef UNICODE
 #define CenterText CenterTextW
+#define TextOut TextOutW
 #else
 #define CenterText CenterTextA
+#define TextOut TextOutA
 #endif
 //test.c
 #ifdef __cplusplus
