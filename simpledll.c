c2a88efda672ebcc73b2224f23048e205fed7383
Aswini S <ashumeow@live.com>
2014-04-28 17:47:14 +0530

    Update simpledll.c

--- simpledll.c
+++ simpledll.c
@@ -2,8 +2,7 @@
 // Not directly copied... The concept of this simple project is inspired from the book of Charles Petzold
 #include<windows.h>
 #include "simpledll.h"
-void main()
-{
+#include<windef.h>
 int WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, PVOID pvReserved)
 {
 	return TRUE;
@@ -23,5 +22,4 @@ export bool CALLBACK CenterTextW(HDC hdc, PRECT prc, PCSTR pString)
 	iLength=lstrlenW(pString);
 	GetTextExtentPoint32W(hdc,pString,iLength,&size);
 	return TextOutW(hdc,(prc->right - prc->left - size.cx)/2,(prc->bottom - prc->top - size.cy)/2,pString,iLength);
-}
 }
