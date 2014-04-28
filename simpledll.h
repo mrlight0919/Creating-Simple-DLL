1e6a2b317d3ba1285d155b292f7a139c246fae9b
Aswini S <ashumeow@live.com>
2014-04-28 17:04:28 +0530

    Update simpledll.h

--- simpledll.h
+++ simpledll.h
@@ -1,10 +1,10 @@
 #ifdef __cplusplus
-#define EXPORT extern "C" __declspec (dllexport)
+#define export extern "C" __declspec (dllexport)
 #else
-#define EXPORT __declspec (dllexport)
+#define export __declspec (dllexport)
 #endif
-EXPORT BOOL CALLBACK CenterTextA (HDC, PRECT, PCSTR)
-EXPORT BOOL CALLBACK CenterTextW (HDC, PRECT, PCWSTR)
+export bool CALLBACK CenterTextA (HDC, PRECT, PCSTR)
+export bool CALLBACK CenterTextW (HDC, PRECT, PCWSTR)
 #ifdef UNICODE
 #define CenterText CenterTextW
 #else
