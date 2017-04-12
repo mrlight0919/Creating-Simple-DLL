e5804c19b09aefe09cbcad1ff8c1b206bb568607
Aswini S <aswini1405@outlook.com>
2017-04-12 12:41:54 +0400

    add updated projects

---
+++ Simple-DLL/DLL-Making/DLL-Making/Module1.vb
@@ -0,0 +1,5 @@
+﻿Public Module Module1
+    Public Function MeowMeow()
+        MsgBox("Meow! Meow! Meow!")
+    End Function
+End Module
---
+++ Simple-DLL/DLL-Making/DLL-Making/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DLL-Making")>
+<Assembly: AssemblyDescription("Basic way of generating a DLL File")>
+<Assembly: AssemblyCompany("Individual - Aswini Sridhar")>
+<Assembly: AssemblyProduct("DLL-Making")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0a99fa22-ca35-401b-a867-ce27206e6d48")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
